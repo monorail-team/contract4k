a71fbc89e2a81441231936b9508346a05f028c57
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-05-19 15:03:00 +0900

    config: build.gradle.kts 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.dokka") version "2.0.0"
     id("io.freefair.aspectj.post-compile-weaving") version "8.4"
 }
 
 group = "io.github.monorail_team.contract4k"
-version = "0.0.10"
+version = "1.0.0"
 
 kotlin {
     jvmToolchain(21)
@@ -19,4 +20,13 @@ dependencies {
     implementation("org.aspectj:aspectjrt:1.9.21")
     implementation("org.slf4j:slf4j-api:2.0.16")
     runtimeOnly("ch.qos.logback:logback-classic:1.5.18")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.named<Jar>("javadocJar") {
+    from(tasks.named("dokkaJavadoc"))
 }
